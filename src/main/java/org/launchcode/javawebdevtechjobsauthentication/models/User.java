d957f2d9c161ac8f63d962aa2f61f4473231b6ea
Sarah <abantaos@hotmail.com>
2020-11-10 13:25:56 -0600

    finish studio

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -0,0 +1,53 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity{
+
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+    public static BCryptPasswordEncoder getEncoder() {
+        return encoder;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwHash() {
+        return pwHash;
+    }
+
+    public void setPwHash(String pwHash) {
+        this.pwHash = pwHash;
+    }
+}
+
